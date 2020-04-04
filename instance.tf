846b6b450fe4a4ac3616a8fd7b8b546dac762ff4
sankamille <luc.brulet@epitech.eu>
2020-04-04 19:17:14 +1100

    [+] create target group, create load balancer in public subnet with a listener that forwared request to the private subnet

--- instance.tf
+++ instance.tf
@@ -38,7 +38,37 @@ resource "aws_launch_configuration" "my_launch_configuration" {
 resource "aws_autoscaling_group" "my_auto_scaling_group" {
   name = "my_auto_scaling_group"
   launch_configuration = aws_launch_configuration.my_launch_configuration.name
+  target_group_arns = [aws_lb_target_group.my_loadbalancer_target_group.arn]
   vpc_zone_identifier = [aws_subnet.private_az01.id, aws_subnet.private_az02.id, aws_subnet.private_az03.id]
   max_size = 1
   min_size = 1
+}
+
+resource "aws_lb_target_group" "my_loadbalancer_target_group" {
+  name = "my-loadbalancer-target-group"
+  port = 80
+  protocol = "HTTP"
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+resource "aws_lb" "my_loadbalancer" {
+  name = "my-loadbalancer"
+  internal = false
+  load_balancer_type = "application"
+  security_groups = [aws_security_group.my_security_group.id]
+  subnets = [aws_subnet.public_az01.id, aws_subnet.public_az02.id, aws_subnet.public_az03.id]
+}
+
+resource "aws_lb_listener" "my_loadbalancer_listener" {
+  load_balancer_arn = aws_lb.my_loadbalancer.arn
+  port = 80
+  protocol = "HTTP"
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.my_loadbalancer_target_group.arn
+  }
+}
+
+output "endpoint" {
+  value = aws_lb.my_loadbalancer.dns_name
 }
