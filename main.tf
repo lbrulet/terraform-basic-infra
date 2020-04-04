fdc14b4db98ea46df376098287042c109e3fea36
sankamille <luc.brulet@epitech.eu>
2020-04-04 17:41:36 +1100

    [+] rename vpc, update default route table to add a route to internet with the gateway

--- main.tf
+++ main.tf
@@ -9,6 +9,10 @@ resource "aws_key_pair" "deployer" {
 
 resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "my_vpc"
+  }
 }
 
 resource "aws_internet_gateway" "my_internet_gateway" {
@@ -16,5 +20,18 @@ resource "aws_internet_gateway" "my_internet_gateway" {
 
   tags = {
     Name = "my_internet_gateway"
+  }
+}
+
+resource "aws_default_route_table" "my_route_table" {
+  default_route_table_id = aws_vpc.my_vpc.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_internet_gateway.id
+  }
+
+  tags = {
+    Name = "my_default_route_table"
   }
 }
