747d900644cfbf20746b9e8b194de84c0a1e96f1
sankamille <luc.brulet@epitech.eu>
2020-04-04 18:02:32 +1100

    [+] create 3 public subnets in 3 different az

---
+++ vpc.tf
@@ -0,0 +1,32 @@
+resource "aws_subnet" "public_az01" {
+  cidr_block = "10.0.0.0/22"
+  vpc_id = aws_vpc.my_vpc.id
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public AZ 01"
+  }
+}
+
+resource "aws_subnet" "public_az02" {
+  cidr_block = "10.0.4.0/22"
+  vpc_id = aws_vpc.my_vpc.id
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public AZ 02"
+  }
+}
+
+resource "aws_subnet" "public_az03" {
+  cidr_block = "10.0.8.0/22"
+  vpc_id = aws_vpc.my_vpc.id
+  availability_zone = "us-east-1c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public AZ 03"
+  }
+}
