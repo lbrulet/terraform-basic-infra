85687067b7811c353363c33fc1a857bd1b0c2e87
sankamille <luc.brulet@epitech.eu>
2020-04-04 18:07:35 +1100

    [+] create 3 data subnets in 3 different az

--- vpc.tf
+++ vpc.tf
@@ -32,7 +32,7 @@ resource "aws_subnet" "public_az03" {
 }
 
 resource "aws_subnet" "private_az01" {
-  cidr_block = "10.0.8.0/22"
+  cidr_block = "10.0.12.0/22"
   vpc_id = aws_vpc.my_vpc.id
   availability_zone = "us-east-1a"
   map_public_ip_on_launch = true
@@ -43,7 +43,7 @@ resource "aws_subnet" "private_az01" {
 }
 
 resource "aws_subnet" "private_az02" {
-  cidr_block = "10.0.8.0/22"
+  cidr_block = "10.0.16.0/22"
   vpc_id = aws_vpc.my_vpc.id
   availability_zone = "us-east-1b"
   map_public_ip_on_launch = true
@@ -54,12 +54,45 @@ resource "aws_subnet" "private_az02" {
 }
 
 resource "aws_subnet" "private_az03" {
-  cidr_block = "10.0.8.0/22"
+  cidr_block = "10.0.20.0/22"
   vpc_id = aws_vpc.my_vpc.id
   availability_zone = "us-east-1c"
   map_public_ip_on_launch = true
 
   tags = {
     Name = "Private AZ 03"
+  }
+}
+
+resource "aws_subnet" "data_az01" {
+  cidr_block = "10.0.24.0/22"
+  vpc_id = aws_vpc.my_vpc.id
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Data AZ 01"
+  }
+}
+
+resource "aws_subnet" "data_az02" {
+  cidr_block = "10.0.28.0/22"
+  vpc_id = aws_vpc.my_vpc.id
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Data AZ 02"
+  }
+}
+
+resource "aws_subnet" "data_az03" {
+  cidr_block = "10.0.32.0/22"
+  vpc_id = aws_vpc.my_vpc.id
+  availability_zone = "us-east-1c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Data AZ 03"
   }
 }
